1c40794566afbbf72c9f4180170fc82f54e84c6f
Amrish Ravidas <aravidas@twitter.com>
2018-10-21 00:33:58 -0700

    Grid click works

--- app/src/main/java/amrish/ravidas/com/alpha/ViewGameGrid.java
+++ app/src/main/java/amrish/ravidas/com/alpha/ViewGameGrid.java
@@ -16,6 +16,9 @@ public class ViewGameGrid extends View {
     private float mFraction = 0.0f;
     private int mCanvasWidth = 0;
     private int mCanvasHeight = 0;
+    private final float[] mRangeX = new float[3];
+    private final float[] mRangeY = new float[3];
+
 
     private final ValueAnimator.AnimatorUpdateListener listener = new ValueAnimator.AnimatorUpdateListener() {
         @Override
@@ -44,6 +47,12 @@ public class ViewGameGrid extends View {
         super.onSizeChanged(w, h, oldW, oldH);
         mCanvasWidth = w;
         mCanvasHeight = h;
+        mRangeX[0] = w / 3;
+        mRangeX[1] = w * 2/3;
+        mRangeX[2] = w;
+        mRangeY[0] = h / 3;
+        mRangeY[1] = h * 2/3;
+        mRangeY[2] = h;
     }
 
     @Override
@@ -63,5 +72,29 @@ public class ViewGameGrid extends View {
         mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
         mPaint.setStrokeJoin(Paint.Join.ROUND);
         mPaint.setStrokeCap(Paint.Cap.ROUND);
+    }
+
+    /**
+     * Given a position in the grid which is virtually divided into 9 section (nonant). This function
+     * returns the index of the section in which the point would belong to. The index starts from top
+     * left corner and increments in steps. For e.g. Position 0x0 is 0, 1x0 is 3, 2x2 is 8
+     */
+    public int getNonantPosition(float x, float y) {
+        int row = -1, col = -1, i = 0, j = 0;
+        while (i < 3) {
+            if (x < mRangeX[i]) {
+                col = i;
+                break;
+            }
+            i++;
+        }
+        while (j < 3) {
+            if (y < mRangeY[j]) {
+                row = j;
+                break;
+            }
+            j++;
+        }
+        return (row >= 0 && col >= 0) ? row * 3 + col : -1;
     }
 }
--- app/src/main/java/amrish/ravidas/com/alpha/ViewTicTacToeGameBoard.java
+++ app/src/main/java/amrish/ravidas/com/alpha/ViewTicTacToeGameBoard.java
@@ -2,10 +2,47 @@ package amrish.ravidas.com.alpha;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.FrameLayout;
 
+import org.jetbrains.annotations.Nullable;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class ViewTicTacToeGameBoard extends FrameLayout {
+
+    private final float[] mLastTouchDownXY = new float[2];
+
+    @BindView(R.id.block_1_1) ViewTicTacToeBlock block_1_1;
+    @BindView(R.id.block_1_2) ViewTicTacToeBlock block_1_2;
+    @BindView(R.id.block_1_3) ViewTicTacToeBlock block_1_3;
+    @BindView(R.id.block_2_1) ViewTicTacToeBlock block_2_1;
+    @BindView(R.id.block_2_2) ViewTicTacToeBlock block_2_2;
+    @BindView(R.id.block_2_3) ViewTicTacToeBlock block_2_3;
+    @BindView(R.id.block_3_1) ViewTicTacToeBlock block_3_1;
+    @BindView(R.id.block_3_2) ViewTicTacToeBlock block_3_2;
+    @BindView(R.id.block_3_3) ViewTicTacToeBlock block_3_3;
+    @BindView(R.id.gameGrid) ViewGameGrid gameGrid;
+
+    View.OnTouchListener touchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+
+            // save the X,Y coordinates
+            if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
+                mLastTouchDownXY[0] = event.getX();
+                mLastTouchDownXY[1] = event.getY();
+            }
+
+            // let the touch event pass on to whoever needs it
+            return false;
+        }
+    };
 
     public ViewTicTacToeGameBoard(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -13,6 +50,47 @@ public class ViewTicTacToeGameBoard extends FrameLayout {
     }
 
     private void init() {
-        LayoutInflater.from(getContext()).inflate(R.layout.game_board, this, true);
+        final View view = LayoutInflater.from(getContext()).inflate(R.layout.game_board, this, true);
+        ButterKnife.bind(this, view);
+        gameGrid.setOnTouchListener(touchListener);
+    }
+
+    @OnClick(R.id.gameGrid)
+    public void onGridClick(View view) {
+        float x = mLastTouchDownXY[0];
+        float y = mLastTouchDownXY[1];
+        int gridPosition = gameGrid.getNonantPosition(x, y);
+        if (gridPosition >= 0) {
+            final ViewTicTacToeBlock block = getBlock(gridPosition);
+            if (block != null) {
+                block.setType(ViewTicTacToeBlock.TYPE.CROSS);
+                block.startAnimator();
+            }
+        }
+    }
+
+    @Nullable
+    private ViewTicTacToeBlock getBlock(int index) {
+        switch (index) {
+            case 0:
+                return block_1_1;
+            case 1:
+                return block_1_2;
+            case 2:
+                return block_1_3;
+            case 3:
+                return block_2_1;
+            case 4:
+                return block_2_2;
+            case 5:
+                return block_2_3;
+            case 6:
+                return block_3_1;
+            case 7:
+                return block_3_2;
+            case 8:
+                return block_3_3;
+        }
+        return null;
     }
 }
