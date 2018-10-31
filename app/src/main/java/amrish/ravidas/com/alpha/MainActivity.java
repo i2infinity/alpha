8af59b742e90734d19da86ce94b2ea0f9b9aee25
Amrish Ravidas <aravidas@twitter.com>
2018-10-31 15:05:49 -0700

    Added fade in-out animations

--- app/src/main/java/amrish/ravidas/com/alpha/MainActivity.java
+++ app/src/main/java/amrish/ravidas/com/alpha/MainActivity.java
@@ -1,13 +1,32 @@
 package amrish.ravidas.com.alpha;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.root_layout)
+    ConstraintLayout mRootConstraintLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        int fullScreenVisibility =
+                View.SYSTEM_UI_FLAG_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_IMMERSIVE;
+        getWindow().getDecorView().setSystemUiVisibility(fullScreenVisibility);
+        final AnimationDrawable animationDrawable = (AnimationDrawable) mRootConstraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(8000);
+        animationDrawable.start();
     }
 }
